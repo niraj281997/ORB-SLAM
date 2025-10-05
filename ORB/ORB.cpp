eb83898352b8ad3e9268459fa3b34e6af955e74f
Niraj_Gohel <37961248+niraj281997@users.noreply.github.com>
2025-10-05 23:32:05 +0530

    Update ORB.cpp

--- ORB/ORB.cpp
+++ ORB/ORB.cpp
@@ -3,6 +3,9 @@
 #include<opencv2/features2d.hpp>
 #include<opencv2/viz.hpp>
 
+using namespace std;
+using namespace cv;
+
 
 /*
 
@@ -15,88 +18,91 @@ Draws the good matches to visualize movement of features between frames.
 Essentially, this is a simple visual tracking setup using ORB.
 */
 
-using namespace std;
-using namespace cv;
-
 int main()
 {
-
-	VideoCapture cap(0);
-	
-	if(!cap.isOpened())
-	{
-		cerr<<"Camera is not getting opened\n";
-	}
-	
-	Ptr<ORB> orb = ORB::create(7000);
-	BFMatcher matcher(NORM_HAMMING);
-	Mat Prev_frame_gray, Prev_desc;
-	vector<KeyPoint>Prev_kp;
-
-	bool F_frame = true;
-	
-	
-	while(true)
-	{
-		Mat Curr_frame, gray,desc;
-		vector<KeyPoint> Curr_kp;
-		cap>>Curr_frame;
-	
-		if(Curr_frame.empty())
-		{
-			cerr<<"The frame is empty\n";
-			break;
-		}
-		cvtColor(Curr_frame, gray, COLOR_BGR2GRAY);
-		
-		//imshow("Check", gray);
-		
-		orb->detectAndCompute(gray,noArray(),Curr_kp,desc);
-
-		if(!F_frame)
-		{
-			vector<DMatch> matches;
-			matcher.match(Prev_desc,desc,matches);
-			
-	
-			double max_dist = 0, min_dist = 100;
-			for(int i = 0;i<Prev_desc.rows; i++)
-			{
-				double distance = matches[i].distance;
-				if(distance < min_dist)
-				min_dist = distance;
-				if(distance > max_dist)
-				max_dist = distance;
+ 	
+VideoCapture cam(1);
+//VideoCapture cam(0);--> to connect the mobile camera
  
-			}
-			vector<DMatch> good_matches;
-			for(auto &m: matches)
-			{
-				if(m.distance <=max(2* min_dist , 30.0))
-				{
-					good_matches.push_back(m);
-				}		
-			}
-		Mat outImg;
-			drawMatches(Prev_frame_gray, Prev_kp, gray,Curr_kp,good_matches, outImg, Scalar::all(-1),Scalar::all(-1),vector<char>(),DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-		imshow("ORB matches", outImg);
-		}
-	
-
-		Prev_kp = Curr_kp;
-		Prev_frame_gray = gray.clone();
-		Prev_desc = desc.clone();
-		F_frame = false;
-
-		char ch = waitKey(2);
-		if(ch=='q' || ch == 27)
-			break;
-		
-	}
-	cap.release();
-	destroyAllWindows();
-
-return 0;
+if(!cam.isOpened())
+{
+	cerr<<"The camera is not getting opened\n";
+    return -1;
+}  
+else 
+{
+	cout<<"Camera got connected"<<endl;
 }
 
+Ptr<ORB> orb = ORB::create(7000);
 
+BFMatcher matcher(NORM_HAMMING);
+
+
+Mat prev_gray, prev_desc, out_image;
+vector<KeyPoint>prev_kp;
+while(true)
+{
+
+	Mat curr_frame, curr_gray, curr_desc;
+	vector<KeyPoint>curr_kp;
+	cam>>curr_frame;
+	
+	if(!curr_frame.empty())
+	{
+		cvtColor(curr_frame, curr_gray,COLOR_BGR2GRAY);
+		//imshow("Camera", curr_frame);
+
+		orb->detectAndCompute(curr_gray,noArray(),curr_kp,curr_desc);
+		
+		if(!prev_desc.empty())
+		{
+			vector<DMatch> matches, good_matches; // each DMatch object represents one pair of matched descriptors (one from previous frame, one from current frame).
+			matcher.match(prev_desc,curr_desc,matches);
+			double max_dist = INT_MIN, min_dist = INT_MAX;
+			for(size_t i = 0 ; i < matches.size(); i++)
+			{
+				double distance = matches[i].distance; //Hamming distance, this is literally "how many bits are different"
+				if(max_dist < distance)
+				{
+					max_dist = distance;	
+				}
+				if(min_dist > distance)
+				{
+					min_dist = distance;
+				}
+			}
+			for(auto &m : matches)
+			{
+				if(m.distance <= max(2* min_dist, 30.0))
+				{
+					good_matches.push_back(m);
+				}
+			}
+			drawMatches(prev_gray, prev_kp, curr_gray,curr_kp,good_matches, out_image, Scalar::all(-1),Scalar::all(-1),vector<char>(),DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+		imshow("ORB matches", out_image);		
+		}
+		prev_kp = curr_kp;
+		prev_gray = curr_gray;
+		prev_desc = curr_desc;
+
+		//prev_gray = curr_gray.clone();
+		//prev_desc = curr_desc.clone();
+		
+		imshow("Camera",curr_gray);
+	}
+
+
+	int key = waitKey(1) & 0xFF;
+	if(key==27 || key=='q' || key=='Q')
+	{
+		break;
+	}
+	else 
+	{
+	}
+}
+	cam.release();
+	destroyAllWindows();
+return 0;
+}
